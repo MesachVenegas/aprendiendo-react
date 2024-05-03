43b2b356ff6002e59de46afae46cf7800d3371f0
mesachman <mesachman@gmail.com>
2024-05-03 09:01:18 +0000

    add: Se agregan extensiones bases usadas en el proyecto

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -5,15 +5,30 @@
   channel = "stable-23.11"; # or "unstable"
   packages = [
     pkgs.nodejs_20
-    pkgs.npm-check
     pkgs.bun
     pkgs.tree
   ];
   # Sets environment variables in the workspace
-  env = {};
+  env = { };
   idx = {
     extensions = [
-      
+      "bradlc.vscode-tailwindcss"
+      "christian-kohler.path-intellisense"
+      "dbaeumer.vscode-eslint"
+      "dsznajder.es7-react-js-snippets"
+      "eamodio.gitlens"
+      "edwinhuish.better-comments-next"
+      "esbenp.prettier-vscode"
+      "formulahendry.auto-close-tag"
+      "GitHub.vscode-pull-request-github"
+      "IronGeek.vscode-env"
+      "miguelsolorio.fluent-icons"
+      "PKief.material-icon-theme"
+      "rangav.vscode-thunder-client"
+      "streetsidesoftware.code-spell-checker"
+      "streetsidesoftware.code-spell-checker-spanish"
+      "usernamehw.errorlens"
+      "wix.vscode-import-cost"
     ];
     # Enable previews
     previews = {
